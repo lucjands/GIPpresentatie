b8a33a750dcde2d3672ae439c7fb298739739719
lds <lucjan.ds@gmail.com>
2016-06-06 15:55:22 +0200

    added contenttable and slides with title for every subject

---
+++ reveal.js-3.3.0/js/jquery.js
@@ -0,0 +1,10 @@
+// Reveal.addEventListener('inhoudanimatie', function() {
+//     console.log("werkt");
+//     $(document.body).click(function() {
+//         $("#test").toggle("slide");
+//     });
+// });
+
+Reveal.addEventListener('inhoudanimatie', function(event) {
+    $("#test").toggle("slide");
+}, false)
