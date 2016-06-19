393f89f143c25b7fa7f1f277798bdfd5434ec004
PvdD <pieter.van.der.deen@skynet.be>
2016-06-19 17:45:14 +0200

    added video

--- reveal.js-3.3.0/js/jquery.js
+++ reveal.js-3.3.0/js/jquery.js
@@ -8,3 +8,13 @@
 Reveal.addEventListener('inhoudanimatie', function(event) {
    $("#test").toggle("slide");
 }, false);
+
+var myVideo = document.getElementById("intro_video");
+Reveal.addEventListener('vid', function(event) {
+   myVideo.play();
+   if (myVideo.mozRequestFullScreen) {
+    myVideo.mozRequestFullScreen();
+  } else if (myVideo.webkitRequestFullScreen) {
+    myVideo.webkitRequestFullScreen();
+  } 
+}, false);
